44bc8af10c0e6d40ea37d604a5e7a912d24d0de2
WON SEOB SEO <11784756+wontheone1@users.noreply.github.com>
2025-05-15 16:15:44 +0300

    Enhance AI integration in the project by adding AI binding in wrangler.jsonc and updating fetch function in index.ts to handle image generation requests based on user prompts.

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,41 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+export interface Env {
+	AI: Ai;
+}
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env): Promise<Response> {
+		const url = new URL(request.url);
+		const prompt = url.searchParams.get('prompt');
+
+		if (!prompt) {
+			return new Response('Prompt is required', { status: 400 });
+		}
+
+		// const steps = parseInt(url.searchParams.get('steps') || '4', 10);
+		const seed = 100;
+
+		const imageStream: ReadableStream<Uint8Array> = await env.AI.run('@cf/bytedance/stable-diffusion-xl-lightning', {
+			prompt,
+			seed,
+		});
+
+		return new Response(imageStream, {
+			headers: {
+				'Content-Type': 'image/jpeg',
+			},
+		});
 	},
-} satisfies ExportedHandler<Env>;
+};
+
+// model to use for beginner: @cf/lykon/dreamshaper-8-lcm (Beta Yes)
+// Stable Diffusion model that has been fine-tuned to be better
+// at photorealism without sacrificing range.
+// model to use for game concept arts: @cf/black-forest-labs/flux-1-schnell (Beta No)
+// FLUX.1 [schnell] is a 12 billion parameter rectified
+// flow transformer capable of generating images from text descriptions.
+// model to use for speed: @cf/bytedance/stable-diffusion-xl-lightning (Beta Yes)
+// SDXL-Lightning is a lightning-fast text-to-image generation model.
+// It can generate high-quality 1024px images in a few steps.
